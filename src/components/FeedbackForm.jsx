08795acff1a93fb211d67d192424eb89bbc62cb5
raymond-io <raymediahub@outlook.com>
2023-12-20 11:23:19 +0100

    complete feedback page development

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
+
+function FeedbackForm({ handleAdd }) {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = ({ target: { value } }) => {
+    if (value === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (value.trim().length < 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(value);
+  };
+
+  const handleSubmit = (e) => {
+    // debugger;
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      handleAdd(newFeedback);
+      setText("");
+    }
+  };
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input
+            name="textfield"
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dr dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dr dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dr dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+  {
+    id: 4,
+    rating: 6,
+    text: "Lorem ipsum dr dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+  {
+    id: 5,
+    rating: 5,
+    text: "Lorem ipsum dr dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+];
+
+export default FeedbackData;
